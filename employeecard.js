fe27873676418d1b6f0d321cf600984118a99f8e
Calvin Turner <turnerjrcalvin@gmail.com>
2022-03-26 15:59:16 -0400

    generate pages

--- employeecard.js
+++ employeecard.js
@@ -10,11 +10,12 @@ employeecard = (data) => {
             myTeam.push(managerCard);
         }
         if (role === 'Engineer') {
-            const engineerCard;
+            const engineerCard= generateEngineer(employee);
+            myTeam.push(engineerCard);
         }
         if (role === 'Intern') {
             const internCard = generateIntern (employee);
-            myTeam.push(internCard)
+            myTeam.push(internCard);
         }
     }
     const employeeCards = myTeam.join('')
@@ -43,7 +44,65 @@ const generateTeamPage = (employeeCards) => {
         ${employeeCards}
         </div>
         </div>
-        
+        <script src="index.js"></script>
         </body>
         </html>`;
 }
+ const generateManager = (manager) => {
+     return `
+     <div class="col">
+     <div class="card">
+         <div class="card-header">
+             <h2>${manager.name}</h2>
+             <h3>Manager<i> <span class="material-icons">
+                 Gathering_spot
+             </span></i></h3>
+         </div>
+         <div class="card-body">
+             <p class="id">ID: ${manager.id}</p>
+             <p class="email">Email: <a href="mailto:${manager.email}">${manager.email}/a></p>
+             <p class="office">Office Number: ${manager.officeNumber}</p>
+         </div>
+     </div>
+ </div>`;
+
+ }
+
+ const generateEngineer = (engineer) => {
+     return `
+     <div class="col">
+                <div class="card">
+                    <div class="card-header">
+                        <h2>${engineer.name}</h2>
+                        <h3>Engineer<i> <span class="material-icons">
+                            Computer_lab
+                        </span></i></h3>
+                    </div>
+                    <div class="card-body">
+                        <p class="id">ID: ${engineer.id}</p>
+                        <p class="email">Email: <a href="mailto:${engineer.email}">${engineer.email}</a></p>
+                        <p class="github">Github: <a href="https://github.com/${engineer.github}">${engineer.github}</a></p>
+                    </div>
+                </div>
+            </div>
+     `
+ }
+ const generateIntern = (intern) => {
+     return `
+     <div class="col">
+     <div class="card">
+         <div class="card-header">
+             <h2>${intern.name}</h2>
+             <h3>Intern<i> <span class="material-icons">
+                 class_room
+             </span></i></h3>
+         </div>
+         <div class="card-body">
+             <p class="id">ID: ${intern.id}</p>
+             <p class="email">Email: <a href="mailto:${intern.email}">${intern.email}</a></p>
+           <p class="school">School: ${intern.school}</p>
+         </div>
+     </div>
+ </div>  `;
+ }
+ module.exports = employeecard;
