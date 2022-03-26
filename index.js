553d3fe6858b01500b5dca3bac29f9fb7a05ff26
Calvin Turner <turnerjrcalvin@gmail.com>
2022-03-26 16:30:28 -0400

    generate cards

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
+const employeecard = require ('./employeecard');
+const Manager = require ('./lib/manager');
+const Engineer = require ('./lib/engineer');
+const intern = require('./lib/intern');
+const
